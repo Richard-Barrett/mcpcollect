c044081e5bde2e718f5d385db6fd3f5b28255e08
Richard Barrett <32808365+Richard-Barrett@users.noreply.github.com>
2020-02-26 09:28:37 -0600

    Update check_stacklight.py

--- Python/check_stacklight.py
+++ Python/check_stacklight.py
@@ -1,1 +1,8 @@
 #!/bin/python
+# ===========================================================
+# Created By: Richard Barrett
+# Organization: Mirantis
+# DepartmenT: L1 Support
+# Purpose: Stacklight Check Log Download Automation
+# Date: 02/26/2020
+# ===========================================================
