786c6895b22b2e19caa92644dfbbc9c318ed8380
Richard Barrett <32808365+Richard-Barrett@users.noreply.github.com>
2020-02-25 21:37:03 -0600

    Create api_cli.py

---
+++ api_cli.py
@@ -0,0 +1,7 @@
+#!/bin/env python
+# ============================================
+# Date Created: Tue Feb 25 21:35:57 CST 2020
+# Author: Richard Barrett
+# Purpose: API Wrapper to Replace Shell Script
+# Comany Affiliation: Mirantis
+# ============================================
