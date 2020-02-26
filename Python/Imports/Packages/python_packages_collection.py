e8b5a28c14466f5172e328320d15e60d5e6853d9
Richard Barrett <32808365+Richard-Barrett@users.noreply.github.com>
2020-02-26 09:31:51 -0600

    Create python_packages_collection.py

---
+++ Python/Imports/Packages/python_packages_collection.py
@@ -0,0 +1,8 @@
+#!/bin/python
+# ===========================================================
+# Created By: Richard Barrett
+# Organization: Mirantis
+# DepartmenT: L1 Support
+# Purpose: Python Packages Log Download Automation
+# Date: 02/26/2020
+# ===========================================================
