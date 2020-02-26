75385f6a3cb35c12a2befb085feda6991cb02a6e
Richard Barrett <32808365+Richard-Barrett@users.noreply.github.com>
2020-02-26 09:38:54 -0600

    Create openstack_compute_migrations.py

---
+++ Python/Automation/Openstack/openstack_compute_migrations.py
@@ -0,0 +1,8 @@
+#!/bin/python
+# ===========================================================
+# Created By: Richard Barrett
+# Organization: Mirantis
+# DepartmenT: L1 Support
+# Purpose: Automated Nova Server Migrations for OS Vms
+# Date: 02/26/2020
+# ===========================================================
