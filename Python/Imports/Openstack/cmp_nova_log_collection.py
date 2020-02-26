ec8c48ed65b0b3bab7dc9fb4114d4d10f9027e88
Richard Barrett <32808365+Richard-Barrett@users.noreply.github.com>
2020-02-26 09:17:43 -0600

    Delete cmp_nova_log_collection.py
    Moved to a different directory

--- Python/Imports/Openstack/cmp_nova_log_collection.py
+++
@@ -1,8 +0,0 @@
-#!/bin/python
-# ===========================================================
-# Created By: Richard Barrett
-# Organization: Mirantis
-# DepartmenT: L1 Support
-# Purpose: Log Download Automation
-# Date: 02/26/2020
-# ===========================================================
