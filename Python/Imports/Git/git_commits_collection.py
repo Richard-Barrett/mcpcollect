d4055b132eb863d2e1101d06a844faf2b3d0fc03
Richard Barrett <32808365+Richard-Barrett@users.noreply.github.com>
2020-02-26 09:33:36 -0600

    Create git_commits_collection.py

---
+++ Python/Imports/Git/git_commits_collection.py
@@ -0,0 +1,8 @@
+#!/bin/python
+# ===========================================================
+# Created By: Richard Barrett
+# Organization: Mirantis
+# DepartmenT: L1 Support
+# Purpose: Git Commits Log Download Automation
+# Date: 02/26/2020
+# ===========================================================
