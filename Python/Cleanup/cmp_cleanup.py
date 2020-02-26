713fc3a488c0849a3f04133094428d4991de8607
Richard Barrett <32808365+Richard-Barrett@users.noreply.github.com>
2020-02-26 09:35:37 -0600

    Create cmp_cleanup.py

---
+++ Python/Cleanup/cmp_cleanup.py
@@ -0,0 +1,8 @@
+#!/bin/python
+# ===========================================================
+# Created By: Richard Barrett
+# Organization: Mirantis
+# DepartmenT: L1 Support
+# Purpose: Compute Cleanup 
+# Date: 02/26/2020
+# ===========================================================
